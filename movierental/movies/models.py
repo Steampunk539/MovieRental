602a0f57ec59f9a02e01f0a9305381e5ffd78cd5
Steampunk539 <nfitowska@gmail.com>
2022-01-24 22:12:36 +0100

    Movies: Improve formatting

--- movierental/movies/models.py
+++ movierental/movies/models.py
@@ -13,6 +13,7 @@ class Author(models.Model):
     def __str__(self):
         return f"{self.name} {self.surname}"
 
+
 class MovieType(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=40)
@@ -21,12 +22,13 @@ class MovieType(models.Model):
     def __str__(self):
         return f"{self.name}"
 
+
 class Movie(models.Model):
-    id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title=models.CharField(max_length=100)
-    author=models.ForeignKey(Author, on_delete=models.CASCADE)
-    type=models.ForeignKey(MovieType, on_delete=models.CASCADE)
-    description=models.CharField(max_length=2000)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    type = models.ForeignKey(MovieType, on_delete=models.CASCADE)
+    description = models.CharField(max_length=2000)
 
     def __str__(self):
         return f"{self.title} {self.author} {self.type}"
